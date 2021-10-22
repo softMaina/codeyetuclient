b3f2696aa0c91dc574588c6487491a53572661e7
softMaina <allancodes@gmail.com>
2021-10-22 05:13:39 +0300

    add build

---
+++ dist/_nuxt/1264b82.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{500:function(e,n,t){"use strict";t.r(n);var r={name:"brandspage"},l=t(60),c=t(66),h=t.n(c),o=t(37),component=Object(l.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("v-sheet",{attrs:{"min-height":"280"}})}),[],!1,null,null,null);n.default=component.exports;h()(component,{VSheet:o.a})}}]);
