1bb3bda7e1b8b3f7e781c90f3c7a91cfc79e1fc0
softMaina <allancodes@gmail.com>
2021-10-25 21:34:13 +0300

    build for production

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - app',
-    title: 'app',
+    titleTemplate: '%s',
+    title: 'Codeyetu',
     htmlAttrs: {
       lang: 'en'
     },
