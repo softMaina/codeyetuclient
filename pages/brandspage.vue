fce9fbb0e5cd6a00c2a4e077fab2905c4b071229
softMaina <allancodes@gmail.com>
2021-11-08 06:01:05 +0300

    update styling

--- pages/brandspage.vue
+++ pages/brandspage.vue
@@ -1,21 +1,28 @@
 <template>
   <v-sheet min-height="280">
 
+
+    <v-expand-transition>
+
+
       <v-row class="mt-16 mb-16">
-       <v-col xl="4" lg="4" md="4" sm="2" xs="2" v-for="brand in brands" :key="brand.brand_id">
-         <v-card class="rounded-xl" elevation="2" @click.stop="getBrand(brand.brand_id)">
-           <div class="d-flex flex-row justify-center" style="height:100%">
+        <v-col xl="4" lg="4" md="4" sm="2" xs="2" v-for="brand in brands" :key="brand.brand_id">
+          <v-card class="rounded-xl" elevation="2" @click.stop="getBrand(brand.brand_id)">
+            <div class="d-flex flex-row justify-center" style="height:100%">
 
-             <v-img class="center" height="200" width="200" aspect-ratio="16/9" contain :src="`${$axios.defaults.baseURL}` + brand.logo"></v-img>
+              <v-img class="center" height="200" width="200" aspect-ratio="16/9" contain
+                     :src="`${$axios.defaults.baseURL}` + brand.logo"></v-img>
 
-           </div>
-           <v-card-title class="justify-center regular-poppins">
-             {{brand.title}}
-           </v-card-title>
-         </v-card>
+            </div>
+            <v-card-title class="justify-center regular-poppins">
+              {{ brand.title }}
+            </v-card-title>
+          </v-card>
 
-      </v-col>
+        </v-col>
       </v-row>
+
+    </v-expand-transition>
 
   </v-sheet>
 </template>
@@ -29,8 +36,8 @@ export default {
   data() {
     return {}
   },
-  created(){
-      this.getBrands();
+  created() {
+    this.getBrands();
   },
   computed: {
     ...mapGetters({
@@ -45,7 +52,7 @@ export default {
       await this.$router.push('/brandoffers')
       await this.$store.dispatch('offers/fetchBrandOffers', id)
     },
-    async getBrands(){
+    async getBrands() {
       await this.$store.dispatch('offers/fetchBrands');
     }
   }
